dae31af7f220d192b2154942d80f1587d0eb7923
JoaoBota <soaresbota@gmail.com>
2022-11-23 13:03:00 +0000

    Links added and added logos to backend

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,12 @@ import { SiNpm } from "react-icons/si";
 import { SiPwa } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiCss3 } from "react-icons/si";
+import { SiPython } from "react-icons/si";
+import { SiJava } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { SiSpringboot } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -39,14 +45,22 @@ export default function Home() {
             <ul className="flex items-center">
               <div className="text-3xl flex justify-center gap-2 text-white">
                 <li>
-                  <AiFillLinkedin />
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/joaobota/"
+                  >
+                    <AiFillLinkedin />
+                  </a>
                 </li>
                 <li>
-                  <AiFillGithub />
+                  <a target="_blank" href="https://github.com/JoaopBota">
+                    <AiFillGithub />
+                  </a>
                 </li>
               </div>
               <li>
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white mr-4 sm:mr-0 px-4 py-1  rounded-xl ml-6"
                   href="#"
                 >
@@ -106,19 +120,20 @@ export default function Home() {
               <Image src={snowfall} />
               <h3 className="text-white text-3xl py-4">SnowCore</h3>
               <p className="text-md mx-8 py-3 leading-8 font-Varela text-zinc-400 text-justify">
-                Software Engineer capable of developing efficient scalable
-                full-stack web-based applications, along with critical and
-                problem-solving
+                Progressive Web Application (PWA) which allows users to see
+                historical snowfall,location, forecast for snowfall in different
+                sky resorts.
               </p>
               <ul className="text-xl flex justify-center gap-5 py-3 text-white">
                 <li>JavaScript</li>
                 <li>React</li>
-                <li>Node</li>
+                <li>API</li>
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/SnowFallData/tree/main/snowfallresort"
                 >
                   Code
                 </a>
@@ -139,8 +154,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/StockPredicter"
                 >
                   Code
                 </a>
@@ -161,8 +177,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/PoisWeb"
                 >
                   Code
                 </a>
@@ -183,8 +200,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/AnimalRescue"
                 >
                   Code
                 </a>
@@ -210,8 +228,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/ChatBot_w-AI"
                 >
                   Code
                 </a>
@@ -234,8 +253,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/MobileApp"
                 >
                   Code
                 </a>
@@ -256,8 +276,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/StormAdviceCenter"
                 >
                   Code
                 </a>
@@ -278,8 +299,9 @@ export default function Home() {
               </ul>
               <div className="py-5">
                 <a
+                  target="_blank"
                   className=" bg-gradient-to-r from-orange-500 to-pink-600 text-white px-4 py-1  rounded-xl ml-6 text-xl"
-                  href="#"
+                  href="https://github.com/JoaopBota/Immersive"
                 >
                   Code
                 </a>
@@ -287,6 +309,7 @@ export default function Home() {
             </div>
           </div>
         </section>
+
         <section>
           <div className="p-10">
             <div className="text-zinc-400 ">
@@ -300,49 +323,94 @@ export default function Home() {
                 </span>
                 , I've done many projects both for business and consumer use.
               </p>
-              </div>
             </div>
-            </section>
+          </div>
+        </section>
 
-            <section >
-              <div className="bg-origins bg-cover">
+        <section>
+          <div className="sm:bg-origins bg-cover">
             <div className="sm:flex">
               <div className="sm:my-16 p-8 text-right">
-              <h4 className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600 text-6xl">Front End</h4>
+                <h4 className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600 text-6xl">
+                  Front End
+                </h4>
               </div>
-
-
-            <div className="grid grid-cols-3 gap-8 text-white text-2xl p-8 justify-items-center ml-4 mr-4 max-w-3xl shadow-2xl shadow-black rounded-xl ">
-                <div><SiJavascript className="text-6xl text-yellow-400 mb-2 2xl:text-8xl"/><p>JavaScript</p></div>
-                <div><SiReact className="text-6xl text-teal-300 mb-2 2xl:text-8xl"/><p className="text-center">React</p></div>
-                <div><SiHtml5 className="text-6xl text-orange-600 mb-2 2xl:text-8xl"/><p>HTML</p></div>
-                <div><SiNextdotjs className="text-6xl text-white mb-2 mr-10 2xl:text-8xl"/><p>Next.js</p></div>
-                <div><SiThreedotjs className="text-6xl text-white mb-2 2xl:text-8xl"/><p className="text-center">Three.js</p></div>
-                <div><SiNpm className="text-6xl text-red-600 mb-2 2xl:text-8xl"/><p className="text-center">NPM</p></div>
-                <div><SiPwa className="text-6xl text-white mb-2 mr-10 2xl:text-8xl"/><p className="2xl:ml-4">PWA</p></div>
-                <div><SiTailwindcss className="text-6xl text-cyan-500 mb-2 2xl:text-8xl"/> <p>Tailwind</p></div>
-                <div><SiCss3 className="text-6xl text-sky-700 mb-2 2xl:text-8xl"/><p className="text-center">CSS</p></div>   
-            </div>
+              <div className="grid grid-cols-3 gap-8 text-white text-2xl p-8 justify-items-center ml-4 mr-4 max-w-3xl shadow-2xl shadow-black rounded-xl ">
+                <div>
+                  <SiJavascript className="text-6xl text-yellow-400 mb-2 2xl:text-8xl" />
+                  <p>JavaScript</p>
+                </div>
+                <div>
+                  <SiReact className="text-6xl text-teal-300 mb-2 2xl:text-8xl" />
+                  <p className="text-center">React</p>
+                </div>
+                <div>
+                  <SiHtml5 className="text-6xl text-orange-600 mb-2 2xl:text-8xl" />
+                  <p p className="text-center">
+                    HTML
+                  </p>
+                </div>
+                <div>
+                  <SiNextdotjs className="text-6xl text-white mb-2 mr-10 2xl:text-8xl" />
+                  <p className="2xl:ml-3">Next.js</p>
+                </div>
+                <div>
+                  <SiThreedotjs className="text-6xl text-white mb-2 2xl:text-8xl" />
+                  <p className="text-center">Three.js</p>
+                </div>
+                <div>
+                  <SiNpm className="text-6xl text-red-600 mb-2 2xl:text-8xl" />
+                  <p className="text-center">NPM</p>
+                </div>
+                <div>
+                  <SiPwa className="text-6xl text-white mb-2 mr-10 2xl:text-8xl" />
+                  <p className="2xl:ml-4">PWA</p>
+                </div>
+                <div>
+                  <SiTailwindcss className="text-6xl text-cyan-500 mb-2 2xl:text-8xl" />{" "}
+                  <p>Tailwind</p>
+                </div>
+                <div>
+                  <SiCss3 className="text-6xl text-sky-700 mb-2 2xl:text-8xl" />
+                  <p className="text-center">CSS</p>
+                </div>
+              </div>
             </div>
             <div className="sm:flex justify-end mt-14">
               <div className="sm:my-16 p-8 text-right">
-              <h4 className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600 text-6xl">Backend</h4>
+                <h4 className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600 text-6xl">
+                  Backend
+                </h4>
               </div>
-
-
-            <div className="grid grid-cols-3 gap-8 text-white text-2xl p-8 justify-items-center ml-4 mr-4 max-w-3xl shadow-2xl shadow-black rounded-xl">
-            <div><SiJavascript className="text-6xl text-yellow-400 mb-2 2xl:text-8xl"/><p>JavaScript</p> </div>
-            <div><SiReact className="text-6xl text-teal-300 mb-2 2xl:text-8xl"/><p>React</p></div>
-            <div><SiHtml5 className="text-6xl text-orange-600 mb-2 2xl:text-8xl"/><p className="text-center">HTML</p></div>
-            <div><SiNextdotjs className="text-6xl text-white mb-2 mr-10 2xl:text-8xl"/><p>Next.js</p></div>
-            <div><SiThreedotjs className="text-6xl text-white mb-2 2xl:text-8xl"/><p className="text-center">Three.js</p></div>
-            <div><SiNpm className="text-6xl text-red-600 mb-2 2xl:text-8xl"/><p className="text-center">NPM</p></div>
+              <div className="grid grid-cols-3 gap-8 text-white text-2xl p-8 justify-items-center ml-4 mr-4 max-w-3xl shadow-2xl shadow-black rounded-xl">
+                <div>
+                  <SiPython className="text-6xl text-slate-400 mb-2 2xl:text-8xl" />
+                  <p className="text-center">Python</p>{" "}
+                </div>
+                <div>
+                  <SiJava className="text-6xl mb-2 2xl:text-8xl" />
+                  <p className="text-center">Java</p>
+                </div>
+                <div>
+                  <SiExpress className="text-6xl text-gray-400 mb-2 2xl:text-8xl" />
+                  <p className="text-center">Express.js</p>
+                </div>
+                <div>
+                  <SiMongodb className="text-6xl text-green-600 mb-2 mr-10 2xl:text-8xl" />
+                  <p>Mongodb</p>
+                </div>
+                <div>
+                  <GrMysql className="text-6xl text-sky-800 mb-2 2xl:text-8xl" />
+                  <p className="text-center">Mysql</p>
+                </div>
+                <div>
+                  <SiSpringboot className="text-6xl text-green-600 mb-2 2xl:text-8xl" />
+                  <p className="text-center">Spring Boot</p>
+                </div>
+              </div>
             </div>
-            </div>
-            </div>
-   
-            </section>
-             
+          </div>
+        </section>
       </main>
     </div>
   );
